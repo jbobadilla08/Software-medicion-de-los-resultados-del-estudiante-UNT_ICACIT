de043341ed8b6291634eeb1ebd8473c7feb4b568
jbobadilla <joalf1994@gmail.com>
2022-06-11 18:02:07 -0500

    Diseño de las interfaces de ingreso de datos

---
+++ src/icacit/Icacit.java
@@ -0,0 +1,19 @@
+
+package icacit;
+
+
+/**
+ *
+ * @author alfie
+ */
+public class Icacit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
